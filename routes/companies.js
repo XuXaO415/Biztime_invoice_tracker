8db3d0a67f700d94b99a3bc8b5320a007c4a792c
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-03-01 20:49:55 -0800

    Added new get route

--- routes/companies.js
+++ routes/companies.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const db = require("../db");
 const slugify = require("slugify");
 
-router.get("/", async(req, res, next) => {
+router.get('/', async(req, res, next) => {
     try {
         const results = await db.query(`SELECT * FROM companies`);
         return res.json({ companies: results.rows });
@@ -12,31 +12,50 @@ router.get("/", async(req, res, next) => {
         return next(e);
     }
 })
-
+router.get('/:code', async(req, res, next) => {
+    try {
+        const { code } = req.params;
+        const cResults = await db.query(`SELECT c.code, c.name, c.description FROM companies AS c WHERE c.code = $1`, [code]);
+        const iResults = await db.query(`SELECT id FROM invoices WHERE comp_code = $1`, [code]);
+        if (cResults.rows.length === 0) {
+            throw new ExpressError(`Company: ${code} not found`, 404)
+        }
+        // const { code, name, description } = cResults.row[0];
+        // const { id, comp_code } = iResults.row[0];
+        const company = cResults.row[0];
+        const invoices = iResults.row[0];
+        company.invoices = invoices.rows.map(i => i.id);
+        return res.json({ company: code, name, description })
+    } catch (e) {
+        return next(e)
+    }
+})
 
 // router.get('/', async function(req, res, next) {
 //     try {
 //         const results = await db.query(`SELECT code, name * FROM companies ORDER BY name`);
 //         return res.json({
-//             companies: results.rows
+//             companies: results.rows[0]
 //         });
 //     } catch (e) {
 //         return next(e);
 //     }
 // })
-router.get('/:code', async function(req, res, next) {
-    try {
-        const { code } = req.params;
-        const results = await db.query(`SELECT code, name, description FROM companies WHERE code = $1`, [code]);
-        if (results.rows.length === 0) {
-            throw new ExpressError(`Company with this ${code} not found`, 404);
-        }
-        console.log(results);
-        return res.json({ companies: results.rows[0] })
-    } catch (e) {
-        return next(e)
-    }
-})
+
+
+// router.get('/:code', async function(req, res, next) {
+//     try {
+//         const { code } = req.params;
+//         const results = await db.query(`SELECT code, name, description FROM companies WHERE code = $1`, [code]);
+//         if (results.rows.length === 0) {
+//             throw new ExpressError(`Company with this ${code} not found`, 404);
+//         }
+//         console.log(results);
+//         return res.json({ companies: results.rows[0] })
+//     } catch (e) {
+//         return next(e)
+//     }
+// })
 
 router.post('/', async(req, res, next) => {
     try {
