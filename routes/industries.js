9ec1d617893a1b1732123fc7300274d6e4e5a7e3
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-03-02 22:03:32 -0800

    Added industry post route

--- routes/industries.js
+++ routes/industries.js
@@ -17,4 +17,34 @@ router.get("/", async(req, res, next) => {
     }
 });
 
+// router.get("/:code", async(req, res, next) => {
+//     try {
+//         const results = await db.query(`SELECT * FROM industries`);
+//         return res.json({
+//             industries: results.rows
+//         });
+//     } catch (e) {
+//         return next(e);
+//     }
+// });
+
+router.post("/", async(req, res, next) => {
+    try {
+        const { code, name } = req.body;
+        const results = await db.query(`INSERT INTO industries (code, name) VALUES ($1, $2) RETURNING code, name`, [code, name]);
+        return res.status(201).json({ industries: results.rows[0] });
+    } catch (e) {
+        return next(e);
+    }
+})
+
+// router.post("/", async(req, res, next) => {
+//     try {
+
+//     } catch (e) {
+//         return next(e);
+//     }
+// })
+
+
 module.exports = router;
